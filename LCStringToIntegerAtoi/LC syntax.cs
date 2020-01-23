f5eda5152300153eed90f26b30015c6f3e2c0f8a
jeremy_plott <plott.j@pg.com>
2020-01-23 15:22:14 -0500

    Add project files.

---
+++ LCStringToIntegerAtoi/LC syntax.cs
@@ -0,0 +1,103 @@
+﻿This one is a little longer because there are a lot of tricky test cases & requirements.
+```
+public class Solution
+{
+    public int MyAtoi(string str)
+    {
+
+        //accounts for any strings without numbers
+        if (!str.Any(char.IsDigit))
+        {
+            return 0;
+        }
+
+        //remove all whitespace from the start
+        str = str.TrimStart(' ');
+
+        //if it starts with a number, +, or -; AND the second character is a number
+        //this is important or cases like "+-4" will return the wrong answer
+        if (char.IsDigit(str[0]) || str[0].Equals('+') && char.IsDigit(str[1]) || str[0].Equals('-') && char.IsDigit(str[1]))
+        {
+            bool isNegative = false;
+
+            //does it start with +/- and if it's a negative number take note of it
+            if (str[0].Equals('-') || str[0].Equals('+'))
+            {
+                if (str[0].Equals('-'))
+                {
+                    isNegative = true;
+                }
+
+                //this takes the rest of the string starting at index 1
+                //trimStart() would remove every instance from the start, so "+++3" would fail
+                str = str.Substring(1);
+            }
+
+            //remove leading zeroes
+            str = str.TrimStart('0');
+            //if the string is now empty or starts with something that isn't a number
+            if (string.IsNullOrEmpty(str) || !char.IsDigit(str[0]))
+            {
+                return 0;
+            }
+
+            string digitString = string.Empty;
+
+            //now that it's been cleaned up, we can assemble the string we'll be converting
+            //this for loop will correspond to the index of each character in the string
+            for (int i = 0; i < str.Length; i++)
+            {
+                //if the character is a number, append it to the empty string made above
+                if (char.IsDigit(str[i]))
+                {
+                    digitString += str[i];
+
+                    //this if statement stops the for loop immediately if it gets more digits 
+                    //than max value, and then returns the appropriate min/max value as requested
+                    if (digitString.Length > 10)
+                    {
+                        if (isNegative)
+                        {
+                            return int.MinValue;
+                        }
+
+                        return int.MaxValue;
+                    }
+
+                    //this try-catch block checks if the next character is a number
+                    //if it isn't a number, or if the next number doesn't exist, it breaks the loop
+                    try
+                    {
+                        if (!char.IsDigit(str[i + 1])) { break; }
+                    }
+                    catch { break; }
+                }
+            }
+
+            //using long instead of int because I only checked if it's over 10 digits long.
+            //some of the test cases will be 10 digits, but more than max value
+            long.TryParse(digitString, out var extractedNumber);
+
+            if (isNegative)
+            {
+                extractedNumber *= -1;
+            }
+            //these two if statements account for everything outside of min/max value range
+            if (extractedNumber > int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+            if (extractedNumber < int.MinValue)
+            {
+                return int.MinValue;
+            }
+
+            //this is cast back to an integer from a long and returned
+            return (int)extractedNumber;
+        }
+
+        //this catches everything else that doesn't start with a number, +, or -
+        return 0;
+    }
+}
+```
---
+++ LCStringToIntegerAtoi/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace LCStringToIntegerAtoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "-3147483648";
+
+            Console.WriteLine(ConvertToInt(str));
+
+            int ConvertToInt(string str)
+            {
+                if (!str.Any(char.IsDigit))
+                {
+                    return 0;
+                }
+
+                str = str.TrimStart(' ');     
+                
+                if (char.IsDigit(str[0]) || str[0].Equals('+') && char.IsDigit(str[1]) || str[0].Equals('-') && char.IsDigit(str[1]))
+                {
+                    bool isNegative = false;
+
+                    if (str[0].Equals('-') || str[0].Equals('+'))
+                    {
+                        if (str[0].Equals('-'))
+                        {
+                            isNegative = true;
+                        }
+
+                        str = str.Substring(1);
+                    }
+
+                    str = str.TrimStart('0');
+
+                    if (string.IsNullOrEmpty(str) || !char.IsDigit(str[0]))
+                    {
+                        return 0;
+                    }
+
+                    string digitString = string.Empty;
+
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (char.IsDigit(str[i]))
+                        {
+                            digitString += str[i];
+
+                            if (digitString.Length > 10)
+                            {
+                                if (isNegative)
+                                {
+                                    return int.MinValue;
+                                }
+
+                                return int.MaxValue;
+                            }
+
+                            try
+                            {
+                                if (!char.IsDigit(str[i + 1])) { break; }
+                            }
+                            catch { break; }
+                        }
+                    }
+
+                    long.TryParse(digitString, out var extractedNumber);
+
+                    if (isNegative)
+                    {
+                        extractedNumber *= -1;
+                    }
+
+                    if (extractedNumber > int.MaxValue)
+                    {
+                        return int.MaxValue;
+                    }
+                    if (extractedNumber < int.MinValue)
+                    {
+                        return int.MinValue;
+                    }
+
+                    return (int)extractedNumber;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
